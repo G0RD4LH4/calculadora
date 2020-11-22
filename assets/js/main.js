8d8aa2f2d130fa22a64af69542ba028f73f9b6a2
G0RD4LH4 <gordalha77@gmail.com>
2020-11-22 01:33:07 -0300

    ...

---
+++ assets/js/main.js
@@ -0,0 +1,56 @@
+function createCalculator() {
+    return {
+        // Atributos:
+        display: document.querySelector('.display'),
+
+        // Metodos:
+        start() {
+            this.click()
+            this.enterPressed()
+        },
+        click() {
+            document.addEventListener('click', event => {
+                const el = event.target
+
+                if(el.classList.contains('button-num')) this.buttonToDisplay(el.innerText)
+                if(el.classList.contains('button-clear')) this.clearDisplay()
+                if(el.classList.contains('button-del')) this.deleteOne()
+                if(el.classList.contains('button-eq')) this.calculate()
+            })
+        },
+        enterPressed() {
+            this.display.addEventListener('keyup', event => {
+                if(event.keyCode === 13) this.calculate()
+            })
+        },
+        buttonToDisplay(value) {
+            this.display.value += value
+        },
+        clearDisplay() {
+            this.display.value = ''
+        },
+        deleteOne() {
+            this.display.value = this.display.value.slice(0, -1)
+        },
+        calculate() {
+            let account = this.display.value
+
+            try {
+                account = eval(account) // ISSO PODE SER MUITO PERIGOSO ... :D
+
+                if(!account) {
+                    alert('Conta inválida')
+                    return
+                }
+
+                this.display.value = account
+            } catch(e) {
+                alert('Conta inválida')
+                return
+            }
+        }
+    }
+}
+
+const calculator = createCalculator()
+calculator.start()
